b13d3f788a13081ffdba316ae623ce29eddfefe6
alumno <alumno@WLPF24ER13.wks.jnj.com>
2024-10-02 10:41:02 -0600

    agregar mas archivos

--- boceto_1/ViewController.swift
+++ boceto_1/ViewController.swift
@@ -10,19 +10,42 @@ import UIKit
 class ViewController: UIViewController {
     var cita_para_enviar: Cita = Cita(quien_lo_dijo: "kyrbo", que_dijo: "poyo")
     var citas_disponibles: GeneradorDeCitas = GeneradorDeCitas()
+    var numero_aleatorio: Int = Int.random(in: 0...25)
+    
+    @IBOutlet weak var labelcito: UILabel!
 
     override func viewDidLoad() {
         citas_disponibles.generar_citas_falsas()
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
-        let valor = Int.random(in: 1...100)
-        Double.random(in: 0...100)
+            
+                super.viewDidLoad()
+                
+                actualizar_cantidad()
     }
+    
+    func actualizar_cantidad(){
+            labelcito.text = String(citas_disponibles.citas_creadas.count)
+        }
 
     @IBSegueAction func al_abrir_pantalla_citas(_ coder: NSCoder) -> ControladorPantallaCita? {
-        return ControladorPantallaCita(cita_para_cita: cita_para_enviar, coder: coder)
+        return ControladorPantallaCita(cita_para_cita: citas_disponibles.obtener_cita_aleatoria(), coder: coder)
+    }
+    
+    /*@IBAction func al_pulsar_boton(_ sender: UIButton){
+        
+    }*/
+    
+    @IBAction func volver_a_pantalla_inicio(segue: UIStoryboardSegue){
+        let pantalla_citas = segue.source as? ControladorPantallaCita
+        print(pantalla_citas?.cita_actual.texto)
+        
+        if let pantalla_citas = segue.source as? ControladorPantallaCita{
+            citas_disponibles.agregar_cita(pantalla_citas.cita_actual.texto, quien_lo_dijo: pantalla_citas.cita_actual.nombre)
+        }
+        
+        actualizar_cantidad()
+        
     }
     
 }
+
 
---
+++ boceto_1/controlador_de_vistas/ControladorGeneradorCitas.swift
@@ -0,0 +1,18 @@
+//
+//  ControladorGeneradorCitas.swift
+//  boceto_1
+//
+//  Created by alumno on 10/2/24.
+//
+
+import Foundation
+
+import UIKit
+
+class ControladorGeneradorCitas: UIViewController{
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
